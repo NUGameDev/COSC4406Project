4b98529d82547db4f20a748cd4557c81f6836dab
Paul Belanger <paulirino@gmail.com>
2015-12-01 12:49:26 -0500

    Implement breath

--- Assets/scripts/movementscript2d.cs
+++ Assets/scripts/movementscript2d.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class movementscript2d : MonoBehaviour
 {
-
+   // public Slider breathslider;
+  //  private float currentbreath;
     Rigidbody rb;
     public float MoveSpeed = 10.0f;
     public float JumpSpeed = 20.0f;
+
+    BreathManager bm;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+      //  currentbreath = breathslider.value;
+        bm = GetComponent<BreathManager>();
     }
 
     // FixedUpdate is called once per physics frame
     void FixedUpdate()
     {
         float move = Input.GetAxisRaw("Horizontal") * MoveSpeed;
-        float jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+        float jump =0f;
+       // if (currentbreath > 0.0f)
+       // {
+             jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+        // }
 
+        if (jump != 0 && bm.CanJump())
+        {
+            bm.BreathJump();
+        }
+        else jump = 0f;
         //c.SimpleMove(Vector3.right * s);
         rb.velocity = new Vector3(move, rb.velocity.y + jump, 0.0f); 
     }
