c3e3afa640b3f0a1982c607bfbdd96277dbbc9c2
Paul Belanger <paulirino@gmail.com>
2015-12-01 14:11:15 -0500

    Breath is pretty much fully defined

---
+++ Assets/scripts/BreathManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Diagnostics;
+using System;
+
+public class BreathManager : MonoBehaviour {
+    public Slider breathslider;
+    private float currentbreath;
+    public float jumpDepletionRate = 5.0f;
+    public float runDepletionRate = 1.0f;
+    public long recoveryWait = 2000;
+    public float recoveryRate = 1f;
+
+    Stopwatch recoveryTimer = new Stopwatch();
+    MovementManager mm;
+    // Use this for initialization
+    void Start () {
+        mm = GetComponent<MovementManager>();
+        currentbreath = breathslider.value;
+        recoveryTimer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Idle breath recovery
+        if (mm.IsMoving())
+        {
+            recoveryTimer.Reset();
+            recoveryTimer.Start();
+        }
+        if (recoveryTimer.ElapsedMilliseconds > recoveryWait)
+        {
+            currentbreath = Math.Min(currentbreath + Time.deltaTime * recoveryRate, breathslider.maxValue);
+        }
+        // running Breath Depletion
+        if (mm.IsRunning())
+        {
+            currentbreath = Math.Max(breathslider.minValue, currentbreath - Time.deltaTime * runDepletionRate);
+        }
+
+        breathslider.value = currentbreath;
+    }
+    public void BreathJump()
+    {
+        currentbreath -= jumpDepletionRate;
+    }
+    public bool CanJump()
+    {
+        return currentbreath >= jumpDepletionRate;
+    }
+}
--- Assets/scripts/MovementManager.cs
+++ Assets/scripts/MovementManager.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class movementscript2d : MonoBehaviour
+public class MovementManager : MonoBehaviour
 {
    // public Slider breathslider;
   //  private float currentbreath;
@@ -10,31 +10,55 @@ public class movementscript2d : MonoBehaviour
     public float MoveSpeed = 10.0f;
     public float JumpSpeed = 20.0f;
 
+    bool grounded = false;
     BreathManager bm;
+    
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
       //  currentbreath = breathslider.value;
         bm = GetComponent<BreathManager>();
+
     }
 
     // FixedUpdate is called once per physics frame
-    void FixedUpdate()
+    void Update()
     {
         float move = Input.GetAxisRaw("Horizontal") * MoveSpeed;
         float jump =0f;
        // if (currentbreath > 0.0f)
        // {
-             jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+        //     jump = Input.GetAxisRaw("Jump") * JumpSpeed;
         // }
-
-        if (jump != 0 && bm.CanJump())
+        if (Input.GetKeyDown("space") && grounded && bm.CanJump())
         {
             bm.BreathJump();
+            jump = JumpSpeed;
         }
-        else jump = 0f;
+      
         //c.SimpleMove(Vector3.right * s);
-        rb.velocity = new Vector3(move, rb.velocity.y + jump, 0.0f); 
+        rb.velocity = new Vector3(move, rb.velocity.y + jump, 0.0f);
+        jump = 0f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Floor"))
+            grounded = true;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        grounded = false;
+    }
+    public bool IsMoving()
+    {
+        return rb.velocity.magnitude > 0.1f;
+    }
+    public bool IsRunning()
+    {
+        return Input.GetAxis("Horizontal") != 0.0f;
     }
 }
