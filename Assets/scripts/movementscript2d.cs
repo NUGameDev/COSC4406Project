33cce815f8206a9b04f8537d25f2dd02d920ff30
Paul Belanger <paulirino@gmail.com>
2015-12-01 11:45:42 -0500

    movement is now somewhat sane

--- Assets/scripts/movementscript2d.cs
+++ Assets/scripts/movementscript2d.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class movementscript2d : MonoBehaviour
 {
 
-    Rigidbody rd;
-    private const float moveSpeed = 10.0f;
+    Rigidbody rb;
+    public float MoveSpeed = 10.0f;
+    public float JumpSpeed = 20.0f;
     // Use this for initialization
     void Start()
     {
-        rd = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // FixedUpdate is called once per physics frame
-    void Update()
+    void FixedUpdate()
     {
-        float s = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float move = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        float jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+
         //c.SimpleMove(Vector3.right * s);
-        rd.AddRelativeForce(new Vector3(0.0f, 0.0f, 1.0f) * s);
-        //Prevent the character from moving in the z direction
-        //transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        rb.velocity = new Vector3(move, rb.velocity.y + jump, 0.0f); 
     }
 }
