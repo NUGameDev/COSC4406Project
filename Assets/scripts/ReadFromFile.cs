e80b34af6cd255c121bcff34a08a463a1475f6b1
hugforakitten <hugforakitten@hotmail.com>
2015-12-15 10:59:08 -0500

    Updated Version of the Help Menu
    Buttons work. Need to work on the text files for editing

---
+++ Assets/scripts/ReadFromFile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+
+public class ReadFromFile : MonoBehaviour {
+    public string TextBox; //reads the text file, and displays it on the screen
+    public string Location; //Variable for the location for the text file.
+    public string TextBoxString;
+
+    void Start()
+    {
+        ReadFile(Location); //starts the functon once the scene or canvas loads in Unity.
+        
+    }
+    void Update()
+    {
+        ReadFile(Location);
+    }
+    public string ReadFile(string Location) //read file function
+    {
+        TextReader textr = new StreamReader(Location); //Reads the text file from a certain location that is specified in Unity. Made for reuse.
+        TextBox = textr.ReadLine(); //reads line by line on the text file \
+        String TextBoxString = "" + TextBox;
+        DisplayText(TextBoxString);
+        return "Test";
+    }
+    public string DisplayText(string TextBoxString)
+    {
+        string Display = "" + TextBox;
+        return Display;
+    }
+}
+
