bd79e7b7e726e8db3a60055815c6e7053955cc7d
hugforakitten <hugforakitten@hotmail.com>
2015-12-17 12:08:07 -0500

    Help menu now has text and can read from files script has been added

--- Assets/scripts/ReadFromFile.cs
+++ Assets/scripts/ReadFromFile.cs
@@ -2,34 +2,31 @@
 using System.Collections;
 using System;
 using System.IO;
-
+using UnityEngine.UI;
 
 public class ReadFromFile : MonoBehaviour {
-    public string TextBox; //reads the text file, and displays it on the screen
+    
     public string Location; //Variable for the location for the text file.
-    public string TextBoxString;
+   
+    public Text t;
+    private string textString;
+
 
     void Start()
     {
-        ReadFile(Location); //starts the functon once the scene or canvas loads in Unity.
-        
+        textString = ReadFile(Location);
     }
     void Update()
     {
-        ReadFile(Location);
+        t.text = textString;
     }
     public string ReadFile(string Location) //read file function
     {
-        TextReader textr = new StreamReader(Location); //Reads the text file from a certain location that is specified in Unity. Made for reuse.
-        TextBox = textr.ReadLine(); //reads line by line on the text file \
-        String TextBoxString = "" + TextBox;
-        DisplayText(TextBoxString);
-        return "Test";
+        TextAsset textr = Resources.Load(Location) as TextAsset; //Reads the text file from a certain location that is specified in Unity. Made for reuse.
+        return textr.text;
+        
+
     }
-    public string DisplayText(string TextBoxString)
-    {
-        string Display = "" + TextBox;
-        return Display;
-    }
+  
 }
 
