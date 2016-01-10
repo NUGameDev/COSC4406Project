3aaa43624cb4bb78bc347f2d2048d08f9732c854
Paul Belanger <paulirino@gmail.com>
2016-01-10 12:25:33 -0500

    another test

--- Assets/Editor/Tests/PlayerManagerTest.cs
+++ Assets/Editor/Tests/PlayerManagerTest.cs
@@ -4,44 +4,62 @@ using UnityEngine;
 [TestFixture]
 public class PlayerManagerTests
 {
+    private GameObject player;
+    private PlayerManager pm;
+
+    [TestFixtureSetUp]
+    public void init()
+    {
+        GameObject player = new GameObject();
+        player.AddComponent<PlayerManager>();
+        pm = player.GetComponent<PlayerManager>();
+    }
+
+    [TestFixtureTearDown]
+    public void finish()
+    {
+        GameObject.DestroyImmediate(player);
+    }
+
     [Test]
     public void BreathIsNeverNegative()
     {
-        //setup
-        GameObject player = new GameObject();
-        player.AddComponent<PlayerManager>();
-        PlayerManager pm = player.GetComponent<PlayerManager>();
         float preBreath = pm.getBreath();
 
         pm.DepleteBreath(2.0f * preBreath);
 
         Assert.That(pm.getBreath() >= 0.0f);
+
     }
     [Test]
     public void BreathNeverOverMaximum()
     {
-        GameObject player = new GameObject();
-        player.AddComponent<PlayerManager>();
-        PlayerManager pm = player.GetComponent<PlayerManager>();
 
         float preBreath = pm.getBreath();
         pm.PufferUseSelf();
 
         Assert.That(pm.getBreath() <= pm.MaxBreath);
+
     }
 
     [Test]
     public void CannotSetBreathPastBounds()
     {
-        GameObject player = new GameObject();
-        player.AddComponent<PlayerManager>();
-        PlayerManager pm = player.GetComponent<PlayerManager>();
 
         pm.setBreath(-1000f);
         Assert.That(pm.getBreath() >= 0.0f);
 
         pm.setBreath(pm.MaxBreath + 10000f);
         Assert.That(pm.getBreath() <= pm.MaxBreath);
+
         
+    }
+    
+    [Test]
+    public void AddScoreIncreasesScore()
+    {
+        int s = pm.getscore();
+        pm.addscore(1);
+        Assert.That(s < pm.getscore());
     }
 }
